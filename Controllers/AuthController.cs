78ab402afcd096ad7060078a3b605df7f3ffd8ea
Daham2000 <daham.akalanka@htown-tech.com>
2023-01-31 22:18:37 +0530

    first commit

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,53 @@
+﻿using FuelIn.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FuelIn.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly AppDbContext _context;
+        public AuthController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View("Login");
+        }
+
+        [HttpPost]
+        public IActionResult Index(string userName, string password) 
+        {
+            EncryptDecryptText encryptDecryptText = new EncryptDecryptText();
+            string encryptedPassword = encryptDecryptText.EncryptText(password);
+            var foundAccount = _context.USER
+                .Where(u => u.USERNAME == userName && u.PASSWORD == encryptedPassword && u.USER_STATUS == "ACT")
+                .FirstOrDefault();
+            if (foundAccount != null)
+            {
+                if (foundAccount.PRIVILEGE_TYPE == "SUPER_ADMIN")
+                {
+                    return View("../Dashboard/SuperAdminDashboard");
+                }
+                else if (foundAccount.PRIVILEGE_TYPE == "MANAGER")
+                {
+                    return View("../Dashboard/ManagerDashboard");
+                }
+                else if (foundAccount.PRIVILEGE_TYPE == "DRIVER")
+                {
+                    return View("../Dashboard/DriverDashboard");
+                }
+                else
+                {
+                    return View("../Dashboard/ConsumerDashboard");
+                }
+            }
+            else 
+            {
+                ModelState.AddModelError("", "Invalid username or password");
+                return View("Login");
+            }
+        }
+    }
+}
---
+++ Models/Auth/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FuelIn.Models.Auth
+{
+    public class User
+    {
+        [Key]
+        public int USER_ID { get; set; }
+        [Required]
+        public string USERNAME { get; set; }
+        [Required]
+        public string PASSWORD { get; set; }
+        [Required]
+        public string PRIVILEGE_TYPE { get; set; } //Hardcoded => Super admin - "SUPER_ADMIN", Station Manager = "MANAGER", Driver = "DRIVER", Consumer = "CONSUMER"
+        [Required] 
+        public string USER_STATUS { get; set; } //Hardcoded => Active = "ACT", Inactive = "INA". INA records are considered to be deleted and unused records.
+    }
+}
