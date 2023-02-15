75496363536369ba4e2292a3e457f410cc50adce
UvinduShehara <sheharauvindu@gmail.com>
2023-02-16 00:48:56 +0530

    Develop the Majority of the Filling station manager section

---
+++ Migrations/20230215101049_nullToken.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FuelIn.Migrations
+{
+    public partial class nullToken : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Token",
+                table: "customerRequests",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "customerRequests",
+                keyColumn: "Token",
+                keyValue: null,
+                column: "Token",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Token",
+                table: "customerRequests",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
